b8bd3a6225329f30f0e02c6415963c5676f1885f
Vibhor <vibhorporwal99@gmail.com>
2019-01-26 00:19:15 +0530

    added HTML generator

---
+++ src/genhtml.py
@@ -0,0 +1,26 @@
+import sys
+import csv
+
+with open('../tests/cfg1', mode='r') as infile:
+    reader = csv.reader(infile)
+    col_spec = {rows[0]:rows[1] for rows in reader}
+print col_spec
+
+outfile = open("output.html","w")
+outfile.write("<html>\n<title>Generated HTML</title>\n<body>\n")
+
+with open('../tests/inp1','r') as f:
+    for line in f:
+        for word in line.split():
+        	if word in col_spec:
+        		outfile.write("<font color = \"")
+        		outfile.write(col_spec[word])
+        		outfile.write("\">")
+        		outfile.write(word)
+        		outfile.write("</font>")
+        		outfile.write(" ")
+        	else:
+        		outfile.write(word)
+        		outfile.write(" ")
+        outfile.write("<br>")
+outfile.write("</body>\n</html>")
