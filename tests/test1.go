26c56a3b3c314c7f0a42382ddc88921e90f9b515
Vibhor <vibhorporwal99@gmail.com>
2019-01-29 18:20:42 +0530

    adding new test

--- tests/test1.go
+++ tests/test1.go
@@ -8,12 +8,16 @@ import (
 var (
 	ToBe   bool       = false
 	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
+	z      complex128 = cmplx.Sqrt(-5.9 + 12iota)
 )
 
+
 func main() {
-	const break = "%T(%v)\n"
-	fmt.Printf(f, ToBe, ToBe)
-	fmt.Printf(f, MaxInt, MaxInt)
+	// lexer didn't fuck up in comments and brackets
+	const x = "%T(x)\n"				// comment
+	fmt.Printf(f, ToBe, ToBe) // Hi this is a comment too
+	// This is as well!		// yo
+	// This is a comment too!
+	fmt.Printf(f, MaxInt, MaxInt)	// yo yo
 	fmt.Printf(f, z, z)
 }
