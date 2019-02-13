3c76b7feb4036a93adc67f94f5ff1b6da071d6ee
Vibhor <vibhorporwal99@gmail.com>
2019-02-14 00:28:22 +0530

    deleted things

---
+++ src/parser/lexer.py
@@ -0,0 +1,100 @@
+import sys
+import csv
+import re
+import argparse
+import ply.lex as lex
+from tokens import *
+from regex import *
+
+ap = argparse.ArgumentParser()                                                                                                                                           
+ap.add_argument("-c", "--cfg",help="configuration")
+ap.add_argument("-i", "--input",help="name of GO input file")
+ap.add_argument("-o", "--output",help="name of output file")
+args = vars(ap.parse_args())
+
+if args["input"] is None:
+    args["input"] = "../../tests/lexer/input/test1.go"
+
+if args["cfg"] is None:
+    args["cfg"] = "../../tests/lexer/cfg/cfg1"
+
+if args["output"] is None:
+    args["output"] = "../../tests/lexer/output/output1.html"
+
+file = args["input"]
+cfgfile = args["cfg"]
+
+with open(cfgfile, mode='r') as infile:
+    reader = csv.reader(infile)
+    col_spec = {rows[0]:rows[1] for rows in reader}
+
+outfile = open(args["output"],"w")
+outfile.write("<html>\n<title>Generated HTML</title>\n<body text=\"white\" bgcolor=\"#4d4d4d\">\n")
+
+mapping = {}
+tabs = []
+
+
+with open(file) as f:
+    for line in f:
+    	# assuming tabs are given for indentation not spaces
+    	tabs.append(len(line) - len(line.lstrip()))
+
+i = 0
+lexer=lex.lex()
+with open(file) as f:
+    code = f.read()
+    code += '\n'
+    lexer.input(code)
+
+num = 0
+
+for tok in lexer:
+	flag = 0
+	if tok.lineno >= (i+1):
+		flag = 1
+		if tok.lineno != 1:
+			for co in range(0,tok.lineno-i):
+				outfile.write("<br>")
+				num = num + 1
+
+		for tab in range(0,tabs[tok.lineno-1]):
+			# assuming 4 &nbsp = 1 Tab
+			outfile.write("&nbsp")
+		num = num + tabs[tok.lineno-1]
+		i = tok.lineno
+
+	if flag == 1:
+		num = tok.lexpos
+	for x in range(0,(tok.lexpos - num)):
+		if tok.type != "STRING":
+			outfile.write("&nbsp")
+		else:
+			if x != 0:
+				outfile.write("&nbsp")
+		num = tok.lexpos
+
+	if tok.type in col_spec:
+		outfile.write("<font color = \"")
+		outfile.write(col_spec[tok.type])
+		outfile.write("\">")
+
+		if tok.type == "STRING":
+			outfile.write("\"")
+		outfile.write(str(tok.value))
+		num = num + len(str(tok.value))
+
+		if tok.type == "STRING":
+			outfile.write("\"")
+			num = num + 2
+		outfile.write("</font>")
+	else:
+		if tok.type == "STRING":
+			outfile.write("\"")
+		outfile.write(str(tok.value))
+		num = num + len(str(tok.value))
+		if tok.type == "STRING":
+			outfile.write("\"")
+			num = num + 2
+
+outfile.write("</body>\n</html>")
---
+++ src/parser/regex.py
@@ -0,0 +1,101 @@
+from tokens import *
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# COMMENTS
+t_COMMENT = r'(/\*([^*]|\n|(\*+([^*/]|\n])))*\*+/)|(//.*)'
+
+#List of all operators in GO
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_MOD     = r'%'
+t_AMPERS  = r'&'
+t_OR 	  = r'\|'
+t_XOR     = r'\^'
+t_SHL     = r'(<<)'
+t_SHR	  = r'(>>)'
+t_AMPCAR  = r'&\^'
+t_PLUSEQ  = r'(\+=)'
+t_MINUSEQ = r'(-=)'
+t_TIMESEQ = r'(\*=)'
+t_DIVIDEEQ= r'/='
+t_MODEQ   = r'(%=)'
+t_AMPEQ   = r'(&=)'
+t_OREQ    = r'(\|=)'
+t_CAREQ   = r'(\^=)'
+t_SHL_ASSIGN    = r'(<<=)'
+t_SHR_ASSIGN    = r'(>>=)'
+t_AMPCAREQ= r'(&\^=)'
+t_AMPAMP  = r'(&&)'
+t_OROR    = r'(\|\|)'
+t_LMINUS  = r'(<-)'
+t_PLUSPLUS= r'(\+\+)'
+t_MINUSMIN= r'(--)'
+t_EQEQ    = r'(==)'
+t_LESS    = r'<'
+t_GREAT   = r'>'
+t_EQUAL   = r'='
+t_NOT     = r'!'
+t_NOTEQ   = r'(!=)'
+t_LEQ     = r'(<=)'
+t_GEQ     = r'(>=)'
+t_COLONEQ = r'(:=)'
+t_DDD	  = r'(\.\.\.)'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LBRACK  = r'\['
+t_RBRACK  = r'\]'
+t_LBRACE  = r'\{'
+t_RBRACE  = r'\}'
+t_COMMA   = r'\,'
+t_DOT     = r'\.'
+t_SEMICOL = r'\;'
+t_COLON   = r'\:'
+
+# Strings in quotes
+def t_STRING(t):
+    r'(\"[^\"]*\")|(\'[^\']*\') '
+    t.value=t.value[1:-1].replace("\'","\"")
+    return t
+
+def t_INTERFACE(t):
+    r'interface'
+    return t
+
+def t_TYPE(t):
+    r'( ((\*)|\ )*int8 | ((\*)|\ )*int16 | ((\*)|\ )*int32 | ((\*)|\ )*int64 | ((\*)|\ )*int |((\*)|\ )*float32 | ((\*)|\ )*float64 | ((\*)|\ )*byte |((\*)|\ )*string | ((\*)|\ )*uintptr | ((\*)|\ )*uint8 | ((\*)|\ )*uint16 | ((\*)|\ )*uint32 | ((\*)|\ )*uint64 | ((\*)|\ )*uint | ((\*)|\ )*bool | ((\*)|\ )*map)'
+    t.value=t.value.replace(" ","")
+    return t
+
+def t_CONSTANTS(t):
+    r'(((\*)|\ )*true|((\*)|\ )*false|((\*)|\ )*iota)'
+    t.value=t.value.replace(" ","")
+    return t
+
+#Identifier token for names and variables
+def t_IDENTIFIER(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'IDENTIFIER')
+    return t
+
+def t_FLOAT(t):
+    r'(([0-9]([0-9]+)*(\.[0-9]([0-9]+)*)?)[eE]\-[0-9]([0-9]+)*)|([0-9]([0-9]+)*\.[0-9]([0-9]+)*)([eE][\+]?[0-9]([0-9]+)*)?'
+    return t
+
+def t_INTEGER(t):
+    r'(0x([0-9A-Fa-f]+)) | [0-9]([0-9]+)*([Ee](\+)?[0-9]([0-9]+)*)?'
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+ERROR_LIST=[]
+
+# Error handling rule
+def t_error(t):
+    ERROR_LIST.append(t.value[0])
+    t.lexer.skip(1)
+
