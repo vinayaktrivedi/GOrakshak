e992ebb2a91e1322424fe674d74768b17b3d64d6
Vibhor <vibhorporwal99@gmail.com>
2019-03-14 23:11:25 +0530

    sfdsf

--- src/3AddressCode/test1.go
+++ src/3AddressCode/test1.go
@@ -5,12 +5,15 @@ import "fmt";
 
 func main(){
 	var a int =10;
+	var b int = 10;
+	b = 11;
+	var d float;
 	var i int;
 	for i=0; i <= 9; i++{
 		var c int =10;
-		a += 10.9;
+		a += 10;
 		break;
 	};
-	a= 90.0*8.1;
+	a, b, d= 90, 567, 90.01;
 	var x float32;
 };
