1a94b7f53fa7d4952f35d9fa631602f4e7bd1a0a
Vibhor <vibhorporwal99@gmail.com>
2019-03-15 00:37:36 +0530

    added value

--- src/3AddressCode/test1.go
+++ src/3AddressCode/test1.go
@@ -4,11 +4,16 @@ import "fmt";
 
 func hello (){
 	var a int = 8;
+	a = 900;
+	var c,d,e int;
+	c,d,e = 2*a,c,10;
 	return 90,a;
 };
 func main(){
 
 	var g [20]int;
 	g[10] = 0;
+	g[9],g[1] = 90,845;
+
 	// g[8] = hello();
 };
