064ad32c8d542ba0da2675045a4d7a32ae65aecc
Vibhor <vibhorporwal99@gmail.com>
2019-03-14 07:02:09 +0530

    sdsd

--- src/3AddressCode/test1.go
+++ src/3AddressCode/test1.go
@@ -6,17 +6,22 @@ var b int = 9 + 9 + 10;
 
 func main(){
 
-	var a int = 5;
-	a += 9 + 78 - 8;
-	a = 9 + 9 - 1;
-	if a == 18{
-		b = 29 + 90;
-		// c = 10; <- there is a error in get_variable_attribute
-	}
-	else{
-		b = 13;
+	var a float;
+	// a += 9 + 78 - 8;
+	// a = 9 + 9 - 1;
+	// if a == 18{
+	// 	b = 29 + 90;
+	// 	// c = 10; <- there is a error in get_variable_attribute
+	// }
+	// else{
+	// 	b = 13;
+	// };
+	// var c int;
+	// // a = 2;
+	// c = b + 2*a - 80 / 2;
+	var i int;
+	for i=0; i <= 9; i++{
+		a += 10.0;
+
 	};
-	var c int;
-	a = 2.8;
-	c = b + 2*a - 80 / 2.22;
 };
