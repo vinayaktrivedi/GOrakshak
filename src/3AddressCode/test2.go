a482c4ec5fc3338c12da1138510870d506f7e594
Vibhor <vibhorporwal99@gmail.com>
2019-03-15 23:16:59 +0530

    sdsd

---
+++ src/3AddressCode/test2.go
@@ -0,0 +1,112 @@
+
+package main;
+import "df";
+import (
+    "fmt";
+    "strings";
+    "adfdf";
+);
+type person struct {
+    name string;
+    age  int;
+};
+func f(a int,b int) int,int {
+	var a int;
+	var b bool;
+};
+func main(){
+
+	var a int ;
+	var b int;
+	var c int;
+	var d int;
+	c,d = f(a,b);
+	var e [3]int;
+	e[2] = 0;
+
+	var i int;
+	i= 2;
+    switch i==1 {
+    case 1:
+        i++;
+    case 2:
+        i--;
+    case 3:
+        i++;
+    default:
+    	i--;
+    };
+	var g [20]int;
+	g[10] = 0;
+	g[9],g[1] := 90,845;
+	var n int;
+	n = +(90+67-90);
+	var k int;
+	k = 90*8;
+	var u **int;
+	u = &(&k);
+	n = **u;
+	n = !90;
+  xy := 2;
+};
+
+// package main;
+// import "fmt";
+// func f(a int,b int) int,int {
+// 	var t int;
+// 	var y int;
+// 	return 2,3;
+// };
+// func main(){
+// 	var a int ;
+// 	var b int;
+// 	var c int;
+// 	var d int;
+// 	c,d = f(a,b);
+// };
+// package main;
+
+// import "df";
+// import (
+//     "fmt";
+//     "strings";
+//     "adfdf";
+// );
+
+
+// type person struct {
+//     name string;
+//     age  int;
+// };
+
+// func main(){
+
+// 	var i int;
+// 	i= 2;
+//   // var s person;
+//   // s = person{name: "Sean", age: 50};
+
+//     switch i==1 {
+//     case 1:
+//         i++;
+//     case 2:
+//         i--;
+//     case 3:
+//         i++;
+//     default:
+//     	i--;
+//     };
+// 	// var g [20]int;
+// 	// g[10] = 0;
+// 	// g[9],g[1] := 90,845;
+// 	// var n int;
+// 	// n = +(90+67-90);
+
+// 	// var k int;
+// 	// k = 90*8;
+// 	// var u **int;
+// 	// u = &(&k);
+// 	// n = **u;
+// 	// n = !90;
+// 	// g[8] = hello();
+// };
