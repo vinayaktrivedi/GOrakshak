1b17ff4a542fb6f8a2072c6a9b13b342dac78897
tushargr <tushar.gurjar1999@gmail.com>
2019-04-15 02:06:15 +0530

    fsadf

--- src/CodeGen/codegen/break_code.py
+++ src/CodeGen/codegen/break_code.py
@@ -1,25 +1,3 @@
 from parameter import *
 
-def findAllBlocks():
-    ret=[]
-    st=0
 
-    for ind, instr in enumerate(ir):
-        flag = 0
-        if instr.type == 'goto' or instr.type == 'ret' or instr.type == 'call':
-            ret.append([st, ind])
-            flag = 1
-
-        elif ind > st and instr.type == 'label':
-            ret.append([st, ind - 1])
-
-        elif ind == len(ir)-1:
-            ret.append((st,ind))
-            flag = 2
-
-        if flag == 1:
-            st = ind + 1
-        if flag == 0:
-            st = ind
-
-    return ret
--- src/CodeGen/codegen/main.py
+++ src/CodeGen/codegen/main.py
@@ -11,6 +11,107 @@ import sys
 import pickle
 
 
+#create blocks
+def findAllBlocks():
+    ret=[]
+    st=0
+
+    for ind, instr in enumerate(ir):
+        if instr.type == 'goto' or instr.type == 'ret' or instr.type == 'call':
+            ret.append([st, ind])
+            st = ind + 1
+
+        elif ind > st and instr.type == 'label':
+            ret.append([st, ind - 1])
+            st = ind
+
+        elif ind == len(ir)-1:
+            ret.append((st,ind))            
+    return ret
+
+#creating next use table for a block
+def createTable(x):
+    start = x[0]
+    end = x[1]
+    ret = {}
+
+    for i in range(start, end + 1):
+        ret[i] = {}
+
+    listOfSymbols = set([])
+    for i in range(start, end + 1):
+        if ir[i].type in type_3:  
+            listOfSymbols.add(ir[i].dst['name'])
+            if((ir[i].src1['type'] == "local") or (ir[i].src1['type'] == "temp") or (ir[i].src1['type'] == "global")): 
+                listOfSymbols.add(ir[i].src1['name'])
+            if((ir[i].src2.type == "local") or (ir[i].src2['type'] == "temp") or (ir[i].src2['type'] == "global")): 
+                listOfSymbols.add(ir[i].src2['name'])
+
+        elif ir[i].type in type_2:
+            listOfSymbols.add(ir[i].dst['name'])
+            if((ir[i].src1['type'] == "local") or (ir[i].src1['type'] == "temp") or (ir[i].src1['type'] == "global")): 
+                listOfSymbols.add(ir[i].src1['name'])
+        
+        elif((ir[i].type in type_5) or (ir[i].type in type_9) or (ir[i].type in type_6)):
+            if((ir[i].src1['type'] == "local") or (ir[i].src1['type'] == "temp") or (ir[i].src1['type'] == "global")): 
+                listOfSymbols.add(ir[i].src1['name'])
+        
+        
+
+    for i in range(start, end + 1):
+        for j in listOfSymbols:
+            ret[i][j] = {}
+            ret[i][j]["live"] = False
+            ret[i][j]["nextUse"] = None
+
+    for i in range(end, start - 1, -1):
+
+        #propogates live and nextUse
+        if i != end:
+            for k in ret[i]:
+                ret[i][k] = ret[i + 1][k].copy()
+
+        if ir[i].type in type_3:
+
+            (ret[i])[ir[i].dst['name']]["live"] = False
+            (ret[i])[ir[i].dst['name']]["nextUse"] = None
+
+            if((ir[i].src1['type'] == "local") or (ir[i].src1['type'] == "temp") or (ir[i].src1['type'] == "global")): 
+                (ret[i])[ir[i].src1['name']]["live"] = True
+                (ret[i])[ir[i].src1['name']]["nextUse"] = i
+
+            if((ir[i].src2['type'] == "local") or (ir[i].src2['type'] == "temp") or (ir[i].src2['type'] == "global")):
+                (ret[i])[ir[i].src2['name']]["live"] = True
+                (ret[i])[ir[i].src2['name']]["nextUse"] = i
+
+        elif ir[i].type in type_2:
+            (ret[i])[ir[i].dst['name']]["live"] = False
+            (ret[i])[ir[i].dst['name']]["nextUse"] = None
+            if((ir[i].src1['type'] == "local") or (ir[i].src1['type'] == "temp") or (ir[i].src1['type'] == "global")): 
+                (ret[i])[ir[i].src1['name']]["live"] = True
+                (ret[i])[ir[i].src1['name']]["nextUse"] = i
+
+        elif ir[i].type in type_5 or ir[i].type in type_6:
+            if((ir[i].src1['type'] == "local") or (ir[i].src1['type'] == "temp") or (ir[i].src1['type'] == "global")): 
+                (ret[i])[ir[i].src1['name']]["live"] = True
+                (ret[i])[ir[i].src1['name']]["nextUse"] = i
+
+        elif ir[i].type in type_9:
+            if((ir[i].src1['type'] == "local") or (ir[i].src1['type'] == "temp") or (ir[i].src1['type'] == "global")): 
+                (ret[i])[ir[i].src1['name']]["live"] = False
+                (ret[i])[ir[i].src1['name']]["nextUse"] = None
+        
+
+    for i in range(start, end):
+        ret[i] = ret[i + 1]
+
+    ## make all live and next Use none for last instruction
+    for i in listOfSymbols:
+        ret[end][i]["live"] = False
+        ret[end][i]["nextUse"] = None
+
+    return ret
+
 #taking input 3ac
 if (len(sys.argv) != 2):
     print("Input 3 Address code not given")
@@ -24,31 +125,31 @@ lines = tuple(open(str(sys.argv[1]), 'r'))
 
 threeAC=[]
 print(lines)
-# for x in lines:
-#     stripped = x.strip().split(",")
-#     for i in range(len(stripped)):
-#         stripped[i] = stripped[i].replace(" ", "")
-#     threeAC.append(stripped)
+for x in lines:
+    stripped = x.strip().split(",")
+    for i in range(len(stripped)):
+        stripped[i] = stripped[i].replace(" ", "")
+    threeAC.append(stripped)
 
-# #taking global symbol table
-# symboltablesfile = open('examplePickle', 'rb')      
-# global_symbol_table = pickle.load(symboltablesfile) 
+#taking global symbol table
+symboltablesfile = open('examplePickle', 'rb')      
+global_symbol_table = pickle.load(symboltablesfile) 
 
 #also attach address in stack of variables, assign type based on local, global, temp,constant
-# for i in threeAC:
-#     ir.append(process.IR(i))
+for i in threeAC:
+    ir.append(process.IR(i))
 
 
-# #finding blocks
-# blocks = blocks.findAllBlocks()
+#finding blocks
+blocks = findAllBlocks()
 
-# #code gen globals, use global_symbol_table
-# generateHelper.genGlobals()
+#code gen globals, use global_symbol_table
+generateHelper.genGlobals()
 
-# #code gen blocks
-# for block in blocks:
-#     nextUseTable = infoTable.createTable(block)
-#     generate.genCodeForBlock(block,nextUseTable)
+#code gen blocks
+for block in blocks:
+    nextUseTable = createTable(block)
+    generate.genCodeForBlock(block,nextUseTable)
 
-# #code gen final
-# generateHelper.close()
+#code gen final
+generateHelper.close()
