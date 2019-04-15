c156c458da8fb7b0a53b1cea00688c1d76b6186f
vinayaktrivedi <vinayakt@iitk.ac.in>
2019-04-16 01:33:19 +0530

    got correct format of globals

--- src/CodeGen/codegen/generateHelper.py
+++ src/CodeGen/codegen/generateHelper.py
@@ -1,10 +1,12 @@
+
 from parameter import *
 
 fil=open('x86.S','w+')
 
 def genGlobals():
-    writeInstr("section	.text")
-    writeInstr("global _start")
+    writeInstr(".section .rodata")
+    writeInstr(".global hello")
+    writeInstr("hello: .string "+"hello world")
     # writeInstr("section	.data")
     # for key, var in global_symbol_table.items():
     #     if(key=="CS335_childtables" or key=="CS335_show_label" or key=="CS335_update_label" or key=="CS335_loop_label" or key=="CS335_exit_label" or key=="CS335_name" or key=="CS335_type" or key=="local_variable_size" or key=="CS335_parent" or key=="CS335_args" or key=="CS335_response"):
